e0446d6484a2446166564b994c283d831ab19766
Ylia Callan 🎼🕸️🪞 <yliacallan@gmail.com>
2025-03-14 17:46:48 +1100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,43 @@
-const cacheName = 'drkarl-pwa-v1';
-const assets = [
-    './',
-    './index.html'
+// ✅ Digital Dr Karl Service Worker
+// This provides offline support for the PWA
+
+const CACHE_NAME = 'dr-karl-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html'
 ];
 
-self.addEventListener('install', (event) => {
-    console.log('[Service Worker] Installing...');
-    event.waitUntil(
-        caches.open(cacheName).then(cache => {
-            console.log('[Service Worker] Caching app shell');
-            return cache.addAll(assets);
-        })
-    );
+self.addEventListener('install', event => {
+  console.log('[Service Worker] Installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('[Service Worker] Caching app shell');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
-    );
+self.addEventListener('activate', event => {
+  console.log('[Service Worker] Activated.');
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          console.log('[Service Worker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  console.log('[Service Worker] Fetch', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
